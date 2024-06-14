231f86621cebfd30494e0367506cec8ad6c82e6a
Gabriel Laguyonie <glaguyon@3E5.42angouleme.fr>
2024-06-14 17:42:43 +0200

    added buffering on file

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,13 +6,17 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 15:04:42 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/06/14 15:06:26 by glaguyon         ###   ########.fr       */
+/*   Updated: 2024/06/14 17:40:11 by glaguyon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
 # define PARSING_H
 
-int	parse_map(void *mlx, t_map *map, int argc, char **argv);
+//do not set to < 2
+# define BUF_SIZE 4096
+
+int		parse_map(void *mlx, t_map *map, int argc, char **argv);
+void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n);
 
 #endif
