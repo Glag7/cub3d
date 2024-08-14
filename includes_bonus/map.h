a15b97d2c55a13aef165ad2ecf61618d4d659133
Gabriel Laguyonie <glaguyon@3E5.42angouleme.fr>
2024-08-14 18:48:20 +0200

    added header new map

--- includes_bonus/map.h
+++ includes_bonus/map.h
@@ -6,7 +6,7 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 14:29:41 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/08/13 16:30:58 by glaguyon         ###   ########.fr       */
+/*   Updated: 2024/08/14 18:45:37 by glaguyon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,30 @@
 # include <stdint.h>
 # include <stddef.h>
 
-# define CUBE 1
-# define GLASS 2
+//full block
+# define CUBE		0b00000000000000000000000000000011
+//door/glass/fence
+# define SPEC		0b00000000000000000000000000001100
+//side to render spec
+# define SIDE		0b00000000000000000000000000010000
+//special value
+# define VALUE		0b11111111111111111111111111100000
+# define VALUEOFF	5
+
+# define XSIDE		0b00000000000000000000000000010000
+# define XSIDE		0b00000000000000000000000000000000
+
+# define GLASS		0b00000000000000000000000000000100
+# define XGLASS		0b00000000000000000000000000010100
+# define YGLASS		0b00000000000000000000000000000100
+
+# define DOOR		0b00000000000000000000000000001000
+# define XDOOR		0b00000000000000000000000000011000
+# define YDOOR		0b00000000000000000000000000001000
+
+# define FENCE		0b00000000000000000000000000001100
+# define XFENCE		0b00000000000000000000000000011100
+# define YFENCE		0b00000000000000000000000000001100
 
 typedef struct s_img
 {
