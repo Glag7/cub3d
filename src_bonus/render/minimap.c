128601edd084a0e49f97978dbef75573f1371a02
Gabriel Laguyonie <glaguyon@3E5.42angouleme.fr>
2024-09-04 19:59:33 +0200

    added flat block to map

--- src_bonus/render/minimap.c
+++ src_bonus/render/minimap.c
@@ -6,7 +6,7 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 18:53:05 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/09/03 16:28:21 by glaguyon         ###   ########.fr       */
+/*   Updated: 2024/09/04 19:57:20 by glaguyon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,36 +16,32 @@
 #include "data.h"
 #include "map.h"
 
-//this sucks
 static inline uint32_t	get_color(t_data *data, int i, int j)
 {
-	double		xx;
-	double		yy;
-	long long	x;
-	long long	y;
+	t_point		pos;
+	t_ipoint	ipos;
+	int			map;
 
-	xx = (data->play.x + (-data->play.cosa
-				* (double)j + data->play.sina * (double)i) / data->set.ratio);
-	yy = (data->play.y + (data->play.cosa
-				* (double)i + data->play.sina * (double)j) / data->set.ratio);
-	x = (long long)floor(xx);
-	y = (long long)floor(yy);
-	if (x < 0LL)
-		x = ((long long)data->map.wid * -x + x) % (long long)data->map.wid;
-	else if (x >= data->map.wid)
-		x %= (long long)data->map.wid;
-	if (y < 0LL)
-		y = ((long long)data->map.hei * -y + y) % (long long)data->map.hei;
-	else if (y >= data->map.hei)
-		y %= (long long)data->map.hei;
-	/*if (data->map.map[(long long)(x + y * data->map.wid)] & SPEC)
-	{
-		if (xx > 0. && yy > 0. && xx < (double)(data->map.wid) && yy < (double)data->map.hei && !(data->tmp.px[(int)((xx - (double)x) * (double)data->tmp.w)
-				+ (int)floor((yy - (double)y) * (double)data->tmp.w) * data->tmp.w] & 0XF0000000))
-		return (data->set.color);
-			return (data->set.ncolor);
-	}*/
-	if (data->map.map[(long long)(x + y * data->map.wid)])
+	pos = (t_point){(data->play.x + (-data->play.cosa * (double)j
+				+ data->play.sina * (double)i) / data->set.ratio),
+		(data->play.y + (data->play.cosa * (double)i + data->play.sina
+				* (double)j) / data->set.ratio)};
+	ipos = (t_ipoint){(long long)floor(pos.x), (long long)floor(pos.y)};
+	if (ipos.x < 0LL)
+		ipos.x = ((long long)data->map.wid * -ipos.x + ipos.x) % (long long)data->map.wid;
+	else if (ipos.x >= data->map.wid)
+		ipos.x %= (long long)data->map.wid;
+	if (ipos.y < 0LL)
+		ipos.y = ((long long)data->map.hei * -ipos.y + ipos.y) % (long long)data->map.hei;
+	else if (ipos.y >= data->map.hei)
+		ipos.y %= (long long)data->map.hei;
+	map = data->map.map[(long long)(ipos.x + ipos.y * data->map.wid)];
+	pos = (t_point){pos.x - ipos.x, pos.y - ipos.y};
+	if (map & CUBE)
+		return (data->set.ncolor);
+	if ((map & SIDE) && (map & SPEC) && pos.x > .45 && pos.x < .55)
+		return (data->set.ncolor);
+	if (!(map & SIDE) && (map & SPEC) && pos.y > .45 && pos.y < .55)
 		return (data->set.ncolor);
 	return (data->set.color);
 }
