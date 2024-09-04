283402674f5d1c6737d5c2169380db6b5e0008a2
Gabriel Laguyonie <glaguyon@3E5.42angouleme.fr>
2024-09-04 17:25:02 +0200

    norme gun

--- src_bonus/parsing/load_gun.c
+++ src_bonus/parsing/load_gun.c
@@ -6,7 +6,7 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 16:55:16 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/09/04 17:09:05 by glaguyon         ###   ########.fr       */
+/*   Updated: 2024/09/04 17:23:51 by glaguyon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,29 @@
 
 int	load_gun(t_data *data)
 {
+	data->map.gun.max = 8;
+	data->map.gun.cur = 0;
 	if (load_img(data->mlx.mlx, TEX_GUN1, data->map.gun.frames + 0,
-				(t_dim){data->set.texsiz, data->set.texsiz}))
+			(t_dim){data->set.texsiz, data->set.texsiz}))
 		return (1);
 	if (load_img(data->mlx.mlx, TEX_GUN2, data->map.gun.frames + 1,
-				(t_dim){data->set.texsiz, data->set.texsiz}))
+			(t_dim){data->set.texsiz, data->set.texsiz}))
 		return (1);
 	if (load_img(data->mlx.mlx, TEX_GUN3, data->map.gun.frames + 2,
-				(t_dim){data->set.texsiz, data->set.texsiz}))
+			(t_dim){data->set.texsiz, data->set.texsiz}))
 		return (1);
 	if (load_img(data->mlx.mlx, TEX_GUN4, data->map.gun.frames + 3,
-				(t_dim){data->set.texsiz, data->set.texsiz}))
+			(t_dim){data->set.texsiz, data->set.texsiz}))
 		return (1);
 	if (load_img(data->mlx.mlx, TEX_GUN5, data->map.gun.frames + 4,
-				(t_dim){data->set.texsiz, data->set.texsiz}))
+			(t_dim){data->set.texsiz, data->set.texsiz}))
 		return (1);
 	if (load_img(data->mlx.mlx, TEX_GUN6, data->map.gun.frames + 5,
-				(t_dim){data->set.texsiz, data->set.texsiz}))
+			(t_dim){data->set.texsiz, data->set.texsiz}))
 		return (1);
 	if (load_img(data->mlx.mlx, TEX_GUN7, data->map.gun.frames + 6,
-				(t_dim){data->set.texsiz, data->set.texsiz}))
+			(t_dim){data->set.texsiz, data->set.texsiz}))
 		return (1);
-	if (load_img(data->mlx.mlx, TEX_GUN8, data->map.gun.frames + 7,
-				(t_dim){data->set.texsiz, data->set.texsiz}))
-		return (1);
-	return (0);
+	return (load_img(data->mlx.mlx, TEX_GUN8, data->map.gun.frames + 7,
+			(t_dim){data->set.texsiz, data->set.texsiz}));
 }
