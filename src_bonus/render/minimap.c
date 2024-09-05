f08849ec4eaf32d0ba1a551703fa34c570afb518
Gabriel Laguyonie <glaguyon@3E5.42angouleme.fr>
2024-09-05 15:07:44 +0200

    minimap done

--- src_bonus/render/minimap.c
+++ src_bonus/render/minimap.c
@@ -6,7 +6,7 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 18:53:05 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/09/04 19:57:20 by glaguyon         ###   ########.fr       */
+/*   Updated: 2024/09/05 14:59:59 by glaguyon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,44 @@
 #include "data.h"
 #include "map.h"
 
+static inline __attribute__((always_inline))
+	void	init_pos(t_data *data, t_point ij, t_point *pos, t_ipoint *ipos)
+{
+	*pos = (t_point){(data->play.x + (-data->play.cosa * ij.y
+				+ data->play.sina * ij.x) / data->set.ratio),
+		(data->play.y + (data->play.cosa * ij.x + data->play.sina
+				* ij.y) / data->set.ratio)};
+	*ipos = (t_ipoint){(long long)floor(pos->x), (long long)floor(pos->y)};
+	if (ipos->x < 0LL)
+		ipos->x = ((long long)data->map.wid * -ipos->x + ipos->x)
+			% (long long)data->map.wid;
+	else if (ipos->x >= data->map.wid)
+		ipos->x %= (long long)data->map.wid;
+	if (ipos->y < 0LL)
+		ipos->y = ((long long)data->map.hei * -ipos->y + ipos->y)
+			% (long long)data->map.hei;
+	else if (ipos->y >= data->map.hei)
+		ipos->y %= (long long)data->map.hei;
+}
+
 static inline uint32_t	get_color(t_data *data, int i, int j)
 {
 	t_point		pos;
 	t_ipoint	ipos;
 	int			map;
 
-	pos = (t_point){(data->play.x + (-data->play.cosa * (double)j
-				+ data->play.sina * (double)i) / data->set.ratio),
-		(data->play.y + (data->play.cosa * (double)i + data->play.sina
-				* (double)j) / data->set.ratio)};
-	ipos = (t_ipoint){(long long)floor(pos.x), (long long)floor(pos.y)};
-	if (ipos.x < 0LL)
-		ipos.x = ((long long)data->map.wid * -ipos.x + ipos.x) % (long long)data->map.wid;
-	else if (ipos.x >= data->map.wid)
-		ipos.x %= (long long)data->map.wid;
-	if (ipos.y < 0LL)
-		ipos.y = ((long long)data->map.hei * -ipos.y + ipos.y) % (long long)data->map.hei;
-	else if (ipos.y >= data->map.hei)
-		ipos.y %= (long long)data->map.hei;
+	init_pos(data, (t_point){i, j}, &pos, &ipos);
 	map = data->map.map[(long long)(ipos.x + ipos.y * data->map.wid)];
 	pos = (t_point){pos.x - ipos.x, pos.y - ipos.y};
 	if (map & CUBE)
 		return (data->set.ncolor);
-	if ((map & SIDE) && (map & SPEC) && pos.x > .45 && pos.x < .55)
+	if ((map & SIDE) && (map & SPEC) && pos.x >= .45 && pos.x <= .55
+		&& ((map & SPEC) != DOOR
+			|| pos.y < 1. - (double)((map & VALUE) >> VALUEOFF) / VALUEONE))
 		return (data->set.ncolor);
-	if (!(map & SIDE) && (map & SPEC) && pos.y > .45 && pos.y < .55)
+	if (!(map & SIDE) && (map & SPEC) && pos.y >= .45 && pos.y <= .55
+		&& ((map & SPEC) != DOOR
+			|| pos.x < 1. - (double)((map & VALUE) >> VALUEOFF) / VALUEONE))
 		return (data->set.ncolor);
 	return (data->set.color);
 }
